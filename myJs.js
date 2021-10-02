a97383ff3c3e76e9274549a342830b74aa49b031
samira talanchi <samira73talanchi@gmail.com>
2021-10-02 16:41:49 +0330

    add animation to description box of menu

---
+++ myJs.js
@@ -0,0 +1,19 @@
+
+function displayFirst() {
+    document.getElementById("first-box").style.animation="showfirst 1s linear ";
+}
+function dNFirst() {
+    document.getElementById("first-box").style.animation="none";
+}
+function displaySecond() {
+    document.getElementById("second-box").style.animation="showsecond 1s linear";
+}
+function dNSecond() {
+    document.getElementById("second-box").style.animation="none";
+}
+function displayThird() {
+    document.getElementById("third-box").style.animation="showthird 1s linear";
+}
+function dNThird() {
+    document.getElementById("third-box").style.animation="none";
+}
